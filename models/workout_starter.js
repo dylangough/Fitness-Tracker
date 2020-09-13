dd6872940e5ddff88ec97fc7b513e166c23e9b91
Dylan Gough <dylangough35@gmail.com>
2020-09-13 14:11:51 -0400

    workout_starter.js

--- models/workout_starter.js
+++ models/workout_starter.js
@@ -5,48 +5,46 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema(
   {
     day: {
-      // For the schema of this attribute, define type and default
       type: Date,
-      default: () => new Date(),
+      default: Date.now
     },
     exercises: [
-      // Schema for each item in exercises array
       {
         type: {
-          // For the schema of this attribute, define type and trim and required
+          type: String,
+          trim: true,
+          required: true
         },
         name: {
-          // For the schema of this attribute, define type and trim and required
+          type: String,
+          trim: true,
+          required: "Enter a name for the exercise"
         },
         duration: {
-          // For the schema of this attribute, define type as number and its required
+          type: Number,
+          required: true
         },
         weight: {
-          // For the schema of this attribute, define type as number
+          type: Number,
         },
         reps: {
-          // For the schema of this attribute, define type as number
+          type: Number,
         },
         sets: {
-          // For the schema of this attribute, define type as number
+          type: Number,
         },
         distance: {
-          // For the schema of this attribute, define type as number
-        },
-      },
-    ],
+          type: Number,
+        }
+      }
+    ]
   },
   {
     toJSON: {
-      // include any virtual properties when data is requested
       virtuals: true,
     },
-  }
-);
-
-// adds a dynamically-created property to schema
+  })
 workoutSchema.virtual('totalDuration').get(function () {
-  // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
